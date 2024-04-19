54d00b27e33435df10e6d85e2c634cf2b9feadde
Logging4J <80828850+Logging4J@users.noreply.github.com>
2024-04-19 14:05:52 -0400

    Update arduinotlight.ino

--- arduinotlight/arduinotlight.ino
+++ arduinotlight/arduinotlight.ino
@@ -1,11 +1,19 @@
-#define RED_PIN 4
-#define GREEN_PIN 2
+#define CW1_RED_PIN 0
+#define CW1_GREEN_PIN 1
+#define CW2_RED_PIN 2
+#define CW2_GREEN_PIN 3
+#define CW3_RED_PIN 4
+#define CW3_GREEN_PIN 5
+#define CW3_RED_PIN 6
+#define CW3_GREEN_PIN 7
+
 
 void setup() {
-  pinMode(RED_PIN, OUTPUT);
-  pinMode(GREEN_PIN, OUTPUT);
+  pinMode(CW1_RED_PIN, OUTPUT);
+  pinMode(CW1_GREEN_PIN, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(RED_PIN, HIGH);
+  digitalWrite(CW1_RED_PIN, HIGH);
 }
+
