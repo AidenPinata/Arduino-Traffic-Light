db49f94d2806e5fd39029ed0acca5221fcdafa20
AidenPinata <pineaide@gmail.com>
2024-04-22 14:00:05 -0400

    Update arduinotlight.ino

--- arduinotlight/arduinotlight.ino
+++ arduinotlight/arduinotlight.ino
@@ -1,19 +1,25 @@
-#define CW1_RED_PIN 7
-#define CW1_YELLOW_PIN 4
-#define CW1_GREEN_PIN 2
-// #define CW2_GREEN_PIN 3
-// #define CW3_RED_PIN 4
-// #define CW3_GREEN_PIN 5
-// #define CW3_RED_PIN 6
-// #define CW3_GREEN_PIN 7
-
+#define TL1_RED_PIN 7
+#define TL1_YELLOW_PIN 4
+#define TL1_GREEN_PIN 2
+#define TL2_RED_PIN 12
+#define TL2_YELLOW_PIN 8
+#define TL2_GREEN_PIN 13
 
 void setup() {
-  pinMode(CW1_RED_PIN, OUTPUT);
-  pinMode(CW1_GREEN_PIN, OUTPUT);
+  pinMode(TL1_RED_PIN, OUTPUT);
+  pinMode(TL1_GREEN_PIN, OUTPUT);
+  pinMode(TL1_YELLOW_PIN, OUTPUT);
+  pinMode(TL2_RED_PIN, OUTPUT);
+  pinMode(TL2_GREEN_PIN, OUTPUT);
+  pinMode(TL2_YELLOW_PIN, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(CW1_RED_PIN, HIGH);
+  digitalWrite(TL1_RED_PIN, HIGH);
+  digitalWrite(TL1_YELLOW_PIN, HIGH);
+  digitalWrite(TL1_GREEN_PIN, HIGH);
+  digitalWrite(TL2_RED_PIN, HIGH);
+  digitalWrite(TL2_YELLOW_PIN, HIGH);
+  digitalWrite(TL2_GREEN_PIN, HIGH);
 }
 
