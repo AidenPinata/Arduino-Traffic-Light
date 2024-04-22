4033c7f119b938e7967726a045f3d90e63f7301e
Logging4J <80828850+Logging4J@users.noreply.github.com>
2024-04-22 13:17:38 -0400

    Update arduinotlight.ino

--- arduinotlight/arduinotlight.ino
+++ arduinotlight/arduinotlight.ino
@@ -1,11 +1,11 @@
-#define CW1_RED_PIN 0
-#define CW1_GREEN_PIN 1
-#define CW2_RED_PIN 2
-#define CW2_GREEN_PIN 3
-#define CW3_RED_PIN 4
-#define CW3_GREEN_PIN 5
-#define CW3_RED_PIN 6
-#define CW3_GREEN_PIN 7
+#define CW1_RED_PIN 7
+#define CW1_YELLOW_PIN 4
+#define CW1_GREEN_PIN 2
+// #define CW2_GREEN_PIN 3
+// #define CW3_RED_PIN 4
+// #define CW3_GREEN_PIN 5
+// #define CW3_RED_PIN 6
+// #define CW3_GREEN_PIN 7
 
 
 void setup() {
