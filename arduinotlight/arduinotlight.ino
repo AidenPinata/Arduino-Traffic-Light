51e19cf920e4cf03d95c72a03ad2c64c26711466
AidenPinata <pineaide@gmail.com>
2024-04-29 13:38:15 -0400

    Update #4

--- arduinotlight/arduinotlight.ino
+++ arduinotlight/arduinotlight.ino
@@ -1,16 +1,32 @@
-#define RED_PIN 7
-#define YELLOW_PIN 4
-#define GREEN_PIN 2
+//Horizontal Traffic lights 
+#define HRED_PIN 7
+#define HYELLOW_PIN 4
+#define HGREEN_PIN 2
+
+//Vertical Traffic light 
+#define VRED_PIN 6
+#define VYELLOW_PIN 5
+#define VGREEN_PIN 3
+
 
 void setup() {
-  pinMode(RED_PIN, OUTPUT);
-  pinMode(GREEN_PIN, OUTPUT);
-  pinMode(YELLOW_PIN, OUTPUT);
+  pinMode(HRED_PIN, OUTPUT);
+  pinMode(HYELLOW_PIN, OUTPUT);
+  pinMode(HGREEN_PIN, OUTPUT);
+
+  pinMode(VRED_PIN, OUTPUT);
+  pinMode(VYELLOW_PIN, OUTPUT);
+  pinMode(VGREEN_PIN, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(RED_PIN, HIGH);
-  digitalWrite(YELLOW_PIN, HIGH);
-  digitalWrite(GREEN_PIN, HIGH);
+  digitalWrite(HRED_PIN, HIGH);
+  digitalWrite(HYELLOW_PIN, HIGH);
+  digitalWrite(HGREEN_PIN, HIGH);
+
+  digitalWrite(VRED_PIN, HIGH);
+  digitalWrite(VYELLOW_PIN, HIGH);
+  digitalWrite(VGREEN_PIN, HIGH);
 }
+
 
