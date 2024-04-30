16e1950f65463a034e0d71c9054fbe054ab9c912
Logging4J <80828850+Logging4J@users.noreply.github.com>
2024-04-30 14:00:38 -0400

    Update #8

--- arduinotlight/arduinotlight.ino
+++ arduinotlight/arduinotlight.ino
@@ -11,13 +11,12 @@
 //Delays
 #define GREEN_DELAY 6000
 #define YELLOW_DELAY 3000
-// #define RED_DELAY 10000
+#define RED_DELAY 2000
 
 void setup() {
   pinMode(HRED_PIN, OUTPUT);
   pinMode(HYELLOW_PIN, OUTPUT);
   pinMode(HGREEN_PIN, OUTPUT);
-
   pinMode(VRED_PIN, OUTPUT);
   pinMode(VYELLOW_PIN, OUTPUT);
   pinMode(VGREEN_PIN, OUTPUT);
@@ -27,7 +26,7 @@ void loop() {
   digitalWrite(HRED_PIN, HIGH);
   digitalWrite(VRED_PIN, HIGH);
   digitalWrite(HGREEN_PIN,LOW);
-  delay(2000);
+  delay(RED_DELAY);
   digitalWrite(HRED_PIN, LOW);
   digitalWrite(HGREEN_PIN, HIGH);
   digitalWrite(VRED_PIN, HIGH);
@@ -38,7 +37,7 @@ void loop() {
   digitalWrite(HYELLOW_PIN, LOW);
   digitalWrite(HRED_PIN, HIGH);
   digitalWrite(VRED_PIN, HIGH);
-  delay(2000);
+  delay(RED_DELAY);
   digitalWrite(VRED_PIN, LOW);
   digitalWrite(VGREEN_PIN, HIGH);
   delay(GREEN_DELAY);
@@ -47,6 +46,7 @@ void loop() {
   delay(YELLOW_DELAY);
   digitalWrite(VYELLOW_PIN, LOW);
   digitalWrite(VRED_PIN, HIGH);
+  delay(YELLOW_DELAY);
 }
 
 
